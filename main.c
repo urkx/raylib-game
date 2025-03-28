c1beb1e7be94d596d9449e1c22de6a8710c801ef
Urko López <urkolopezlopez@gmail.com>
2025-03-28 20:58:15 +0100

    Done

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include <stdio.h>
 
 /**
  * Taken from raylib [textures] example - Background scrolling
@@ -10,20 +11,25 @@ int main(int argc, char* argv[])
 
 	// Initialization
     //--------------------------------------------------------------------------------------
-    // const int screenWidth = GetScreenWidth();
-    // const int screenHeight = GetScreenHeight();
-    const int screenWidth = 652*2;
-    const int screenHeight = 358*2;
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
+    //const int screenWidth = 652*2;
+    //const int screenHeight = 358*2;
 
-    InitWindow(screenWidth, screenHeight, "raylib [textures] example - background scrolling");
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(screenWidth, screenHeight, "Calendario vacaciones");
 
     // ToggleFullscreen();
 
     // NOTE: Be careful, background width must be equal or bigger than screen width
     // if not, texture should be draw more than two times for scrolling effect
-    Texture2D background = LoadTexture("bu.webp.jpeg");
+    Texture2D background = LoadTexture("2025-lista.jpg");
+    Texture2D header = LoadTexture("2025-header.jpg");
 
     float scrollingBack = 0.0f;
+    float headerInit = 0.0f;
+
+    const int verticalOffset = 100; 
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -33,10 +39,10 @@ int main(int argc, char* argv[])
     {
         // Update
         //----------------------------------------------------------------------------------
-        scrollingBack -= 5.0f;
+        scrollingBack -= 2.5f;
 
         // NOTE: Texture is scaled twice its size, so it sould be considered on scrolling
-        if (scrollingBack <= -background.width*2) scrollingBack = 0;
+        if (scrollingBack <= -background.width) scrollingBack = 0;
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -47,11 +53,9 @@ int main(int argc, char* argv[])
 
             // Draw background image twice
             // NOTE: Texture is scaled twice its size
-            DrawTextureEx(background, (Vector2){ scrollingBack, 0 }, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 0 }, 0.0f, 2.0f, WHITE);
-
-            DrawText("BACKGROUND SCROLLING & PARALLAX", 10, 10, 20, RED);
-            DrawText("(c) Cyberpunk Street Environment by Luis Zuno (@ansimuz)", screenWidth - 330, screenHeight - 20, 10, RAYWHITE);
+            DrawTextureEx(background, (Vector2){ scrollingBack + 357.0f, verticalOffset }, 0.0f, 1.0f, WHITE);
+            DrawTextureEx(background, (Vector2){ background.width + scrollingBack + 357.0f, verticalOffset }, 0.0f, 1.0f, WHITE);
+            DrawTextureEx(header, (Vector2){ headerInit, verticalOffset }, 0.0f, 1.0f, WHITE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -60,6 +64,7 @@ int main(int argc, char* argv[])
     // De-Initialization
     //--------------------------------------------------------------------------------------
     UnloadTexture(background);  // Unload background texture
+    UnloadTexture(header);
 
     CloseWindow();              // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
